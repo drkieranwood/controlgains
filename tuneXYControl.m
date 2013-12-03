a2ed0bdce6852b4cc17e5757dd8217aa66ca3f18
unknown <user@IT051692.users.bris.ac.uk>
2013-12-03 17:51:49 +0000

    Gains changed

--- tuneXYControl.m
+++ tuneXYControl.m
@@ -122,7 +122,7 @@ linkaxes([ax1 ax2 ax3],'x');
 % Cost and noise (tuning matrices)
 %=============================================
 %Noise standard distributions
-inputNoise = 0.01;
+inputNoise = 0.001;
 if ptam_on
     measuNoise = sqrt(0.0039);
 else
